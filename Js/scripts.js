5c3726749e8b7eab3719e9bdcc7fc9bff77fbe24
D4ni11 <d4ni11c.a@gmail.com>
2025-09-04 23:21:28 -0300

    agregado de productos y cambio de moneda

--- Js/scripts.js
+++ Js/scripts.js
@@ -6,10 +6,10 @@ const products = [
   { id: 4, name: "Notebook DELL Latitude 7390", price: 428, image: "https://www.infinit.com.uy/imgs/productos/productos33_38679.jpg" },
   { id: 5, name: "Notebook HP 15-fc0037wm", price: 480, image: "https://www.infinit.com.uy/imgs/productos/productos33_36527.jpg" },
   { id: 6, name: "APPLE Macbook Air BMT12LL/A 2017", price: 501, image: "https://www.infinit.com.uy/imgs/productos/productos33_34109.jpg" },
-  { id: 7, name: "Notebook ASUS Vivobook Go 15 E1504GA-NJ535W", price: 799, image: "https://www.infinit.com.uy/imgs/productos/productos33_38450.jpg" },
-  { id: 8, name: "Notebook DELL Latitude 3420", price: 1199, image: "https://www.infinit.com.uy/imgs/productos/productos33_31003.jpg" },
-  { id: 9, name: "Equipo PC INTEL Core i3 13100 13va Gen", price: 199, image: "https://www.infinit.com.uy/imgs/productos/productos3_38323.jpg" },
-  { id: 10, name: "PC Gamer AMD Ryzen 7 5700G", price: 249, image: "https://www.infinit.com.uy/imgs/productos/productos3_19809.jpg" }
+  { id: 7, name: "Notebook ASUS Vivobook Go 15 E1504GA-NJ535W", price: 547, image: "https://www.infinit.com.uy/imgs/productos/productos33_38450.jpg" },
+  { id: 8, name: "Notebook DELL Latitude 3420", price: 613, image: "https://www.infinit.com.uy/imgs/productos/productos33_31003.jpg" },
+  { id: 9, name: "Equipo PC INTEL Core i3 13100 13va Gen", price: 576, image: "https://www.infinit.com.uy/imgs/productos/productos3_38323.jpg" },
+  { id: 10, name: "PC Gamer AMD Ryzen 7 5700G", price: 636, image: "https://www.infinit.com.uy/imgs/productos/productos3_19809.jpg" }
 ];
 
 // ==== MOSTRAR PRODUCTOS EN LA GRILLA ====
@@ -21,7 +21,7 @@ function displayProducts() {
     card.innerHTML = `
       <img src="${p.image}" alt="${p.name}">
       <h3>${p.name}</h3>
-      <p>$${p.price}</p>
+      <p> USD ${p.price}</p>
       <div class="product-actions">
        <a href="productos.html" class="btn-ver-mas"> <button>Ver más</button> </a>
       </div>
